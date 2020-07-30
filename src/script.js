387681a2981469f4cdb1a6d7aee20f826239745b
edf10 <eduardamirelly404@outlook.com>
2020-07-30 11:26:50 -0300

    :triangular_flag_on_post: update input validation

--- src/script.js
+++ src/script.js
@@ -15,6 +15,10 @@ document.querySelector('button').onclick = function convertBin2Dec(){
 
 function validationNumber(valueDigit){
     for(let cont = 0; cont<valueDigit.length; cont++){
-        if(valueDigit[cont] != 1 && valueDigit[cont] != 0) document.getElementById('bin').value = null;
+        if(valueDigit[cont] != 1 && valueDigit[cont] != 0){
+            alert('Please! Enter only 0 or 1!'); 
+            document.getElementById('bin').value = null;
+            break;
+        }
     }
 }
